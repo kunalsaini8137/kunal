31c5b0add994f3604ee9fe823f9647d96f35d231
kunalsaini8137 <51744880+kunalsaini8137@users.noreply.github.com>
2020-04-15 12:45:01 +0530

    Add files via upload
    This is my project on password security system.I have used algorithm like SHA 256,Salting,Hashing e.t.c.

--- main.c
+++ main.c
@@ -1,355 +1,293 @@
 #include<stdio.h>
-#include<memory.h>
-#include<stdio.h>
-#include<string.h>//strcmp(),strcat()
-#include<stdlib.h>//rand()
-#include<time.h>//time(0)
-#include<ctype.h>
-#include "PS_SHA_256.c"
-#include "aes.c"
-
-#define MAX_LIMIT 1000
-
-
-
-
-
-void signup(char *username, char *password);
-int verifyPassword(int ,char *);
-int verifyUsername(char *);
-char* generateSalt();
-char salt[5];
-char* getSalt(int sNo);
-//void file_encrypt();
-//uc* struprr(uc *);
-
-
-
-
-int main()
+#include<stdlib.h>
+#include<string.h>
+// The number of columns comprising a state in AES. This is a constant in AES. Value=4
+#define Nb 4
+#define chunk 1024
+// The number of rounds in AES Cipher. It is simply initiated to zero. The actual value is received in the program.
+int Nr=0;
+// The number of 32 bit words in the key. It is simply initiated to zero. The actual value is received in the program.
+int Nk=0;
+// in - it is the array that holds the plain text to be encrypted.
+// out - it is the array that holds the key for encryption.
+// state - the array that holds the intermediate results during encryption.
+unsigned char in[16], out[16], state[4][4];
+// The array that stores the round keys.
+unsigned char RoundKey[240];
+// temp stores the key
+// temp2 stores the plaintext
+unsigned char temp[32] = {0x54  ,0x68  ,0x61  ,0x74  ,0x73  ,0x20  ,0x6D  ,0x79  ,0x20  ,0x4B  ,0x75  ,0x6E  ,0x67  ,0x20  ,0x46  ,0x75};
+unsigned char temp2[64]= {0x54  ,0x77  ,0x6F  ,0x20  ,0x4F  ,0x6E  ,0x65  ,0x20  ,0x4E  ,0x69  ,0x6E  ,0x65  ,0x20  ,0x54  ,0x77  ,0x6F};
+unsigned char temp3[64];
+// The Key input to the AES Program
+unsigned char Key[32];
+int hex_to_int(char c)
 {
-    int choice,check;
-    char username[50],password[50];
-
-
-    printf("\n**************Password Security System******************************\n");
-    printf("\n**************1. Signin(Existing User)****************************** \n");
-    printf("\n**************2. Signup(New User)*********************************** \n");
-    printf("\n******************************************************************** \n");
-
-
-    scanf("%d",&choice);
-
-    if(choice==1)
-    {
-        int sNo;
-        printf("\n Enter your Username:\n");
-        printf("\n**********************\n");
-        scanf("%s",username);
-        sNo=verifyUsername(username);
-        if(sNo)
-        {
-            printf("\n Username verified.\n");
-            printf(" \n******************** \n");
-            printf("\n Enter Your Password:\n");
-            printf("\n************************ \n");
-            scanf("%s",password);
-
-           if(verifyPassword(sNo,password))
-            {
-                printf("\n Login Successful\n");
-                printf("\n******************\n");
-
-            }
-            else
-            {
-                printf("\n Wrong Password\n");
-                printf("\n******************\n");
-
-            }
+    if (c >= 97)
+        c = c - 32;
+    int first = c / 16 - 3;
+    int second = c % 16;
+    int result = first * 10 + second;
+    if (result > 9) result--;
+    return result;
+}
+int hex_to_ascii(char c, char d){
+        int high = hex_to_int(c) * 16;
+        int low = hex_to_int(d);
+        return high+low;
+}
+void strip(char *s) {
+    char *p2 = s;
+    while(*s != '\0') {
+        if(*s != '\t' && *s != '\n') {
+            *p2++ = *s++;
+        } else {
+            ++s;
         }
-        else
-        {
-            printf("No such user exists.\n");
-            printf("\n******************\n");
-        }
-
     }
-
-    else if(choice==2)
-    {
-       reu: printf("\n Enter your Username:\n");
-        printf("\n**********************\n");
-        scanf("%s",username);
-        check= (int)(username[0]);
-        if(strlen(username)<3 || (check>=48 &&check <=57) )
-           {
-               printf("username should be of at least 3 characters or should not start with a number re-enter \n");
-               goto reu;
-           }
-
-        rep:printf("\n Enter Your Password:\n");
-        printf("\n************************ \n");
-        scanf("%s",password);
-        if(strlen(password)<8)
-        {
-            printf("Your password is too weak..!!Enter password of atleast 8 characters..\n ");
-            goto rep;
-        }
-
-        signup(username,password);
-        printf("\n Signed up Successfully:\n");
-        printf("\n************************ \n");
-    }
-
-    else
-    {
-        printf("Invalid choice! Try again.");
-        printf("\n************************ \n");
-    }
-
-    //file_encrypt();
-    return 0;
+    *p2 = '\0';
 }
 
 
-
-
-/*uc* struprr(uc *h1)
+int getSBoxValue(int num)
 {
-    int i;printf("Converting to upper case \n\n");char ch;
-    for(i=0;i<32;i++)
-    {
-        h1[i]=toupper(h1[i]);
-        printf("%02x ",ch);
-        //printf("%c ",toupper("a"));
-    }
-        for(i=0;i<32;i++)
-    {
-        //h1[i]=toupper(h1[i]);
-        printf("%02x ",h1[i]);
-        //printf("%c ",toupper("a"));
-    }
-
-    return h1;
-} */
-
-char* generateSalt()
-{
-    srand(time(0));
-    short index;
-    int i;
-    FILE *fp_salt_directory;
-    char arr[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q'
-    ,'r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9',
-    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-    for (i=0;i<4;i++)
-    {
-        index=rand()%62;
-        salt[i]=arr[index];
-    }
-    salt[4]='\0';
-    fp_salt_directory=fopen("salt_directory.txt","a");
-    fputs(salt,fp_salt_directory);
-    fputs("\n",fp_salt_directory);
-    fclose(fp_salt_directory);
-
-    return salt;
+	int sbox[256] =   {
+	//0     1    2      3     4    5     6     7      8    9     A      B    C     D     E     F
+	0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76, //0
+	0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0, //1
+	0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15, //2
+	0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75, //3
+	0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84, //4
+	0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf, //5
+	0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8, //6
+	0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2, //7
+	0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73, //8
+	0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb, //9
+	0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79, //A
+	0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08, //B
+	0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a, //C
+	0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e, //D
+	0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf, //E
+	0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16 }; //F
+	return sbox[num];
 }
 
+// The round constant word array, Rcon[i], contains the values given by
+// x to th e power (i-1) being powers of x (x is denoted as {02}) in the field GF(28)
+// Note that i starts at 1, not 0).
+int Rcon[255] = {
+	0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a,
+	0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39,
+	0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a,
+	0x74, 0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8,
+	0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef,
+	0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc,
+	0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b,
+	0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3,
+	0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94,
+	0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20,
+	0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97, 0x35,
+	0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f,
+	0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04,
+	0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63,
+	0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd,
+	0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb  };
 
-void signup(char *username,char *password)
+// This function produces Nb(Nr+1) round keys. The round keys are used in each round to encrypt the states.
+void KeyExpansion()
 {
-    char *salt_pointer,buf=0;;uc hash[32],h2[32], *h1, chh =' ';
-    int i,num;
-    SHA2 ptr;
-    memset(hash,0,sizeof(hash));
+	int i,j;
+	unsigned char temp[4],k;
 
-    FILE *fp_user_directory,*fp_pwd_directory1,*fp_backup;
-    FILE *fp_pwd_directory;
-    fp_user_directory=fopen("user_directory.txt","a");
-    fputs(username,fp_user_directory);
-    fputs("\n",fp_user_directory);
-    fclose(fp_user_directory);
+	// The first round key is the key itself.
+	for(i=0;i<Nk;i++)
+	{
+		RoundKey[i*4]=Key[i*4];
+		RoundKey[i*4+1]=Key[i*4+1];
+		RoundKey[i*4+2]=Key[i*4+2];
+		RoundKey[i*4+3]=Key[i*4+3];
+	}
 
-      salt_pointer=generateSalt();
-    //printf("Salt pointer is initilized \n\n");
+	// All other round keys are found from the previous round keys.
+	while (i < (Nb * (Nr+1)))
+	{
+					for(j=0;j<4;j++)
+					{
+						temp[j]=RoundKey[(i-1) * 4 + j];
+					}
+					if (i % Nk == 0)
+					{
+						// This function rotates the 4 bytes in a word to the left once.
+						// [a0,a1,a2,a3] becomes [a1,a2,a3,a0]
 
-      strcat(password,salt_pointer);
-    //printf("password is modified %s salt \n\n",password);
-      fp_pwd_directory1=fopen("PWD.txt","a");
-      fputs(password,fp_pwd_directory1);
-      fputs("\n",fp_pwd_directory1);
-      fp_backup=fopen("User_Backup.txt","a");
-      fputs(username,fp_backup);
-      fputs("\n",fp_backup);
-      fclose(fp_backup);
+						// Function RotWord()
+						{
+							k = temp[0];
+							temp[0] = temp[1];
+							temp[1] = temp[2];
+							temp[2] = temp[3];
+							temp[3] = k;
+						}
 
+						// SubWord() is a function that takes a four-byte input word and
+						// applies the S-box to each of the four bytes to produce an output word.
 
-    // Hash one
-      initilize(&ptr);
-      update(&ptr,password,strlen(password));
-      data_final(&ptr,hash);
-      //printf("Printng the contents using standard hash function for storing purpose \n\n");
-     //h1=print_hash(hash);
-      //h1= struprr(h1);
+						// Function Subword()
+						{
+							temp[0]=getSBoxValue(temp[0]);
+							temp[1]=getSBoxValue(temp[1]);
+							temp[2]=getSBoxValue(temp[2]);
+							temp[3]=getSBoxValue(temp[3]);
+						}
 
-
-    fp_pwd_directory=fopen("pwd_directory.txt","a");
-    fputs(hash,fp_pwd_directory);
-    fputs("\n",fp_pwd_directory);
-    //fprintf(fp_pwd_directory,"%02x",h1);
-    fclose(fp_pwd_directory);
-    fclose(fp_pwd_directory1);
-
-    //fp_pwd_directory=fopen("pwd_directory.txt","r");
-
-    //printf("Printing hash after returning from SHA2 function \n\n");
-
-    /*for(i=0;i<32;i++)
-    {
-      //h1[i]= fscanf(fp_pwd_directory,"%02x",h2);
-       chh+=h1[i];
-       //strcat(chh,h1[i]);
-       printf("%x ",chh);
-
-      //printf("%02x",h1[i]);
-
-    }
-    printf("\n");
-
-    /*for(i=0;i<32;i++)
-    {
-      //h1[i]= fscanf(fp_pwd_directory,"%02x",db_pwd);
-      printf("%02x ",h1[i]);
-
-    } */
-
-
+						temp[0] =  temp[0] ^ Rcon[i/Nk];
+					}
+					else if (Nk > 6 && i % Nk == 4)
+					{
+						// Function Subword()
+						{
+							temp[0]=getSBoxValue(temp[0]);
+							temp[1]=getSBoxValue(temp[1]);
+							temp[2]=getSBoxValue(temp[2]);
+							temp[3]=getSBoxValue(temp[3]);
+						}
+					}
+					RoundKey[i*4+0] = RoundKey[(i-Nk)*4+0] ^ temp[0];
+					RoundKey[i*4+1] = RoundKey[(i-Nk)*4+1] ^ temp[1];
+					RoundKey[i*4+2] = RoundKey[(i-Nk)*4+2] ^ temp[2];
+					RoundKey[i*4+3] = RoundKey[(i-Nk)*4+3] ^ temp[3];
+					i++;
+	}
 }
 
-
-
-int verifyUsername(char *user_query)
+// This function adds the round key to state.
+// The round key is added to the state by an XOR function.
+void AddRoundKey(int round)
 {
-    int sNo=0;
-    char db_username[50];
-    FILE *fp_user_directory;
-    fp_user_directory=fopen("user_directory.txt","r");
-
-    while(fscanf(fp_user_directory,"%s",db_username)!=EOF)
-    {
-        sNo++;
-        if(strcmp(db_username,user_query)==0)
-        {
-            fclose(fp_user_directory);
-            return sNo;
-        }
-    }
-    fclose(fp_user_directory);
-    return 0;
+	int i,j;
+	for(i=0;i<4;i++)
+	{
+		for(j=0;j<4;j++)
+		{
+			state[j][i] ^= RoundKey[round * Nb * 4 + i * Nb + j];
+		}
+	}
 }
 
-
-
-int verifyPassword(int sNo,char *pwd_query)
+// The SubBytes Function Substitutes the values in the
+// state matrix with values in an S-box.
+void SubBytes()
 {
-    int i;
-    char *hash_query;char db_salt[5];SHA2 ptr;uc hash[32],*h1 ,h2[32],db_pwd[50];
-    FILE *fp_pwd_directory,*fp_pwd_directory1;FILE *fp_salt_directory;
-    memset(hash,0,sizeof(hash));
-    memset(h2,0,sizeof(h2));
-    memset(db_pwd,0,sizeof(db_pwd));
+	int i,j;
+	for(i=0;i<4;i++)
+	{
+		for(j=0;j<4;j++)
+		{
+			state[i][j] = getSBoxValue(state[i][j]);
 
-
-    fp_salt_directory=fopen("salt_directory.txt","r" );
-    fp_pwd_directory1=fopen("PWD.txt","r" );
-    for(i=0;i<sNo;i++)
-    {
-            fscanf(fp_salt_directory,"%s",db_salt);
-             fscanf(fp_pwd_directory1,"%s",db_pwd);
-    }
-
-    strcat(pwd_query,db_salt);
-    //hash_query=generate_hash(pwd_query);
-
-    // Hash one
-      initilize(&ptr);
-      update(&ptr,pwd_query,strlen(pwd_query));
-      data_final(&ptr,hash);
-
-      //h1= print_hash(hash);
-     // printf(" \n\n Printng the contents using standard hash function matching purpose in verification function \n\n");
-
-
-    //fp_pwd_directory=fopen("pwd_directory.txt","r");
-    //printf(" \n %s \n",h1);
-
-    /*for(i=0;i<32;i++)
-    {
-      h2[i]= fscanf(fp_pwd_directory,"%02x",db_pwd);
-      //printf("%02x",h1[i]);
-
-    }*/
-    //printf("\n\n printing contents from file \n\n");
-    //fscanf(fp_pwd_directory,"%s",db_pwd);
-    //printf("%s \n",db_pwd);
-
-    /*for(i=0;i<32;i++)
-    {
-      //h1[i]= fscanf(fp_pwd_directory,"%02x",db_pwd);
-      printf("%02x ",h2[i]);
-
-    } */
-
-
-    if(strcmp(pwd_query,db_pwd)==0)
-        {
-            fclose(fp_pwd_directory1);
-            return 1;
-        }
-
-    /*for(i=0;i<sNo;i++)
-    {
-        fscanf(fp_pwd_directory,"%s",db_pwd);
-        if(strcmp(db_pwd,hash)==0)
-        {
-            fclose(fp_pwd_directory);
-            return 1;
-        }
-    } */
-    fclose(fp_pwd_directory1);
-    return 0;
+		}
+	}
 }
-/*int main()
+
+// The ShiftRows() function shifts the rows in the state to the left.
+// Each row is shifted with different offset.
+// Offset = Row number. So the first row is not shifted.
+void ShiftRows()
 {
-    uc text1[MAX_LIMIT],hash[32];
-    printf("Enter your data yo be hashed \n");
-    gets(text1);
+	unsigned char temp;
 
+	// Rotate first row 1 columns to left
+	temp=state[1][0];
+	state[1][0]=state[1][1];
+	state[1][1]=state[1][2];
+	state[1][2]=state[1][3];
+	state[1][3]=temp;
 
-   int idx;
-   SHA2 ptr;
+	// Rotate second row 2 columns to left
+	temp=state[2][0];
+	state[2][0]=state[2][2];
+	state[2][2]=temp;
 
-   // Hash one
-   //initilize(&ptr);
-   //update(&ptr,text1,strlen(text1));
-   //data_final(&ptr,hash);
-   //print_hash(hash);
+	temp=state[2][1];
+	state[2][1]=state[2][3];
+	state[2][3]=temp;
 
-   signup("aryan",text1);
-
-
-   getchar();
-   return 0;
+	// Rotate third row 3 columns to left
+	temp=state[3][0];
+	state[3][0]=state[3][3];
+	state[3][3]=state[3][2];
+	state[3][2]=state[3][1];
+	state[3][1]=temp;
 }
-*/
 
-/*void file_encrypt()
+// xtime is a macro that finds the product of {02} and the argument to xtime modulo {1b}
+#define xtime(x)   ((x<<1) ^ (((x>>7) & 1) * 0x1b))
+
+// MixColumns function mixes the columns of the state matrix
+void MixColumns()
 {
-    int i,len;
+	int i;
+	unsigned char Tmp,Tm,t;
+	for(i=0;i<4;i++)
+	{
+		t=state[0][i];
+		Tmp = state[0][i] ^ state[1][i] ^ state[2][i] ^ state[3][i] ;
+		Tm = state[0][i] ^ state[1][i] ; Tm = xtime(Tm); state[0][i] ^= Tm ^ Tmp ;
+		Tm = state[1][i] ^ state[2][i] ; Tm = xtime(Tm); state[1][i] ^= Tm ^ Tmp ;
+		Tm = state[2][i] ^ state[3][i] ; Tm = xtime(Tm); state[2][i] ^= Tm ^ Tmp ;
+		Tm = state[3][i] ^ t ; Tm = xtime(Tm); state[3][i] ^= Tm ^ Tmp ;
+	}
+}
 
-	 // Key size
+// Cipher is the main function that encrypts the PlainText.
+void Cipher()
+{
+	int i,j,round=0;
+
+	//Copy the input PlainText to state array.
+	for(i=0;i<4;i++)
+	{
+		for(j=0;j<4;j++)
+		{
+			state[j][i] = in[i*4 + j];
+		}
+	}
+
+	// Add the First round key to the state before starting the rounds.
+	AddRoundKey(0);
+
+	// There will be Nr rounds.
+	// The first Nr-1 rounds are identical.
+	// These Nr-1 rounds are executed in the loop below.
+	for(round=1;round<Nr;round++)
+	{
+		SubBytes();
+		ShiftRows();
+		MixColumns();
+		AddRoundKey(round);
+	}
+
+	// The last round is given below.
+	// The MixColumns function is not here in the last round.
+	SubBytes();
+	ShiftRows();
+	AddRoundKey(Nr);
+
+	// The encryption process is over.
+	// Copy the state array to output array.
+	for(i=0;i<4;i++)
+	{
+		for(j=0;j<4;j++)
+		{
+			out[i*4+j]=state[j][i];
+		}
+	}
+}
+  void main()
+{
+	int i,len;
+
+	Nr=128; // Key size
 
 	// Calculate Nk and Nr from the given Nr value.
 	Nk = Nr / 32;
@@ -362,7 +300,7 @@ int verifyPassword(int sNo,char *pwd_query)
     char outword[100000];
     file = fopen("User_Backup.txt", "r");
     if (buf == NULL) {
-
+    /* deal with malloc() failure */
     }
    if(file)
 {
@@ -372,7 +310,7 @@ int verifyPassword(int sNo,char *pwd_query)
 
 
     if (ferror(file)) {
-
+        /* deal with error */
     }
     fclose(file);
 }
@@ -386,7 +324,7 @@ int verifyPassword(int sNo,char *pwd_query)
           printf("\nArray elements are : \n");
 
           memcpy(temp2,buf,len);
-          /*for(i=0;i<len;i++)
+          for(i=0;i<len;i++)
           {
               printf("Hex: %#04X\n",temp2[i]);
           }
@@ -402,18 +340,18 @@ int verifyPassword(int sNo,char *pwd_query)
 
 // The KeyExpansion routine must be called before encryption.
 	KeyExpansion();
-    /*printf("\n Plaintext:\n");
+    printf("\n Plaintext:\n");
     for(i=0;i<Nk*4;i++)
 	{
 
 		printf("%02x ",in[i]);
-	} */
+	}
 	// The next function call encrypts the PlainText with the Key using AES algorithm.
-	//Cipher();
+	Cipher();
 
 
 	// Output the encrypted text.
-	/*printf("\nHexadecimal after encryption:\n");
+	printf("\nHexadecimal after encryption:\n");
 
 	for(i=0;i<Nk*4;i++)
 	{
@@ -438,5 +376,4 @@ int verifyPassword(int sNo,char *pwd_query)
 	}
    printf("File created : ubackup.txt");
    fclose(f);
-   }*/
-
+   }
